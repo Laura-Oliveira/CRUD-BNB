8b96f6e7e7e673882aaf0893d53a7e1dd6101097
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-08-09 00:56:10 -0300

    [Create] BackEnd tipo de usuario que sera cadastrado

--- app/src/main/java/com/app/crud/controller/RegisterTypeUserActivity.java
+++ app/src/main/java/com/app/crud/controller/RegisterTypeUserActivity.java
@@ -2,15 +2,30 @@ package com.app.crud.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.app.crud.R;
 
-public class RegisterTypeUserActivity extends AppCompatActivity {
-
+public class RegisterTypeUserActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_type_user);
+    }
+
+    public void openRegisterUserPage(View view)
+    {
+        Intent intent = new Intent(this, RegisterUserActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRegisterCompanyPage(View view)
+    {
+        Intent intent = new Intent(this, RegisterUserCompanyActivity.class);
+        startActivity(intent);
     }
 }
